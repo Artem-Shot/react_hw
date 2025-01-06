6ca3b84ea9c7a3dffb1b64641fbda2bb18497d88
Artem <artemshotskiy@gmail.com>
2025-01-06 16:21:23 +0200

    hw-50 done

---
+++ my-react-app/components/FuncComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FuncComponent(props) {
+    return <h1>Привіт, {props.name}</h1>
+}
+
+export default FuncComponent
---
+++ my-react-app/components/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return <h1 className="qwerty"><b>Hello world!</b></h1>;
+    }
+}
+
+export default Hello;
