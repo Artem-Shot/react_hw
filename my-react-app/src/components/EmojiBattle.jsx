72c5ededb5ae46e9045b5c4afbe4704628e16222
Artem <artemshotskiy@gmail.com>
2025-01-22 16:01:30 +0200

    hw-52 in done

--- my-react-app/src/components/EmojiBattle.jsx
+++ my-react-app/src/components/EmojiBattle.jsx
@@ -1,6 +1,6 @@
 import {Component} from 'react';
 import emoji from '../data/emoji.js';
-import {ListGroup} from "react-bootstrap";
+import {ListGroup, Col, Button} from "react-bootstrap";
 
 class EmojiBattle extends Component {
     constructor() {
@@ -8,25 +8,54 @@ class EmojiBattle extends Component {
         this.state = {
             clickedEmoji: emoji.reduce((acc, item) => {
                 acc[item.id] = 0
-                return acc
-            })
+                return acc;
+            },{})
 
         }
 
     }
 
+    clickHandler = (id) => {
+        this.setState((first) => ({
+            clickedEmoji: {
+                ...first.clickedEmoji,
+                [id]: first.clickedEmoji[id] + 1,
+            }
+        }));
+    };
+
+    getWinner = () => {
+        const {clickedEmoji} = this.state;
+        const maxVotes = Math.max(...Object.values(clickedEmoji))
+        const winner = Object.keys(clickedEmoji).find(
+            (id) => clickedEmoji[id] === maxVotes
+        );
+        return emoji.find((item) => +item.id === +winner).emoji;
+    };
     render() {
-        console.log(emoji);
+        const {clickedEmoji} = this.state;
         return (
             <div>
                 <ListGroup>
                     {emoji.map((item) => {
-                        return  <ListGroup.Item key={item.id}>
-                            {item.emoji}
-                            </ListGroup.Item>
+                        return  <ListGroup.Item
+                            key={item.id}
+                            onClick={() => this.clickHandler(item.id)}
+                        >
+                            {item.emoji} — Votes: {clickedEmoji[item.id]}
+                        </ListGroup.Item>
                         })}
                 </ListGroup>
+
+                <Button
+
+                    onClick={() => alert(`Winner: ${this.getWinner()}`)}
+                >
+                    Results
+                </Button>
            </div>
+
+
         );
     }
 }
