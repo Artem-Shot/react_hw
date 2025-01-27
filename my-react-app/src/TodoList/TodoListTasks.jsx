b4e93e8bb1f319c0caae2dd4b56c0b4d08444237
Artem <artemshotskiy@gmail.com>
2025-01-27 11:31:23 +0200

    hw-53  done

--- my-react-app/src/TodoList/TodoListTasks.jsx
+++ my-react-app/src/TodoList/TodoListTasks.jsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 const TodoListTasks = ({data}) => {
     return (
         <ListGroup className="mt-4">
+            {/* eslint-disable-next-line react/prop-types */}
             {data && data.map(({title, description}) => {
                 return <ListGroup.Item key={uuidv4()} className='mb-3'>
                     <div className='text-bg-warning'><b>TITLE: {title}</b></div>
