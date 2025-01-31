e6a6b6d4475553e93d51c4663d2ef3be09faf9f1
Artem <artemshotskiy@gmail.com>
2025-01-31 19:50:37 +0200

    hw-55  done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,42 +1,70 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 
-const Basic = () => (
+const MyForm = () => (
     <div>
-        <h1>Any place in your app!</h1>
+        <h1>Fill up</h1>
         <Formik
-            initialValues={{ email: '', password: '' }}
-            validate={values => {
+            initialValues={{
+                firstName: '',
+                phone: '',
+                email: '',
+            }}
+            validate={(values) => {
                 const errors = {};
-                if (!values.email) {
-                    errors.email = 'Required';
-                } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                    errors.email = 'Invalid email address';
+
+
+                if (!values.firstName) {
+                    errors.firstName = "First name is required";
                 }
+
+
+                if (!values.phone) {
+                    errors.phone = "Phone number is required";
+                } else if (values.phone.length !== 12 || isNaN(values.phone)) {
+                    errors.phone = "Phone number must be exactly 12 digits";
+                }
+
+                // Валідація email
+                if (!values.email) {
+                    errors.email = "Email is required";
+                } else if (!values.email.includes("@")
+                ) {
+                    errors.email = "Invalid email format required @";
+                }
+
                 return errors;
             }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
+            onSubmit={async (values, { setSubmitting }) => {
+                await new Promise((r) => setTimeout(r, 500));
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
             }}
         >
             {({ isSubmitting }) => (
-                <Form>
-                    <Field type="email" name="email" />
-                    <ErrorMessage name="email" component="div" />
-                    <Field type="password" name="password" />
-                    <ErrorMessage name="password" component="div" />
-                    <button type="submit" disabled={isSubmitting}>
-                        Submit
-                    </button>
-                </Form>
-            )}
+            <Form>
+                <label htmlFor="firstName">First Name</label>
+                <Field id="firstName" name="firstName" placeholder="Jane" />
+                <ErrorMessage name="firstName" component="div" className="error" />
+
+                <label htmlFor="phone"> Phone</label>
+                <Field id="phone" name="phone" placeholder="phone number" />
+                <ErrorMessage name="phone" component="div" className="error" />
+
+                <label htmlFor="email"> Email</label>
+                <Field
+                    id="email"
+                    name="email"
+                    placeholder="jane@acme.com"
+                    type="email"
+                />
+                <ErrorMessage name="email" component="div" className="error" />
+
+                <button type="submit" disabled={isSubmitting}>Submit</button>
+            </Form>
+                )}
         </Formik>
     </div>
 );
 
-export default Basic;
+export default MyForm;
